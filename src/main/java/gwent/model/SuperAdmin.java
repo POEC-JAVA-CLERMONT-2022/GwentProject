0849ffb18528bf85b681071bf968707a0858cc1d
ipmeSL <100273771+ipmeSL@users.noreply.github.com>
2022-03-22 16:09:08 +0100

    first pass on user classes

---
+++ src/main/java/gwent/model/SuperAdmin.java
@@ -0,0 +1,10 @@
+package gwent.model;
+
+public class SuperAdmin extends Admin {
+
+	public SuperAdmin(String username, String email, String password) {
+		super(username, email, password);
+		// TODO Auto-generated constructor stub
+	}
+
+}
