fb5ad6d1ec28a79fe9bc93e30a73e4daac32b424
maoherve <maoherve8@gmai.com>
2022-05-04 12:03:20 +0200

    merge

--- api/src/main/java/com/example/gwent_projet/entity/user/ERole.java
+++ api/src/main/java/com/example/gwent_projet/entity/user/ERole.java
@@ -1,7 +1,7 @@
 package com.example.gwent_projet.entity.user;
 
 public enum ERole {
-	// user role not necessary? this is supposed to be account-less
+
     ROLE_USER,
     ROLE_MODERATOR,
     ROLE_ADMIN
